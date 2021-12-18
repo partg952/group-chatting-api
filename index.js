ce493ac2c3b7bbbae450e3eee36479b6b3dac659
dhr231 <dhruvprashar231@gmail.com>
2021-12-18 12:31:57 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const app = require("express")();
+const cors = require("cors")
+const server = require("http").createServer(app);
+const socket = require("socket.io")(server,{
+cors:{origin:"*"}
+})
+
+
+app.get("/",(req,res)=>{
+	res.send("hello world")
+})
+
+
+socket.on("connection",data=>{
+	data.on("join_room",room=>{
+		data.join(room)
+		console.log(`${data.id} has joined room ${room}`)
+	})
+	data.on("message",message=>{
+		console.log(`${data.id} said ${message.text}`)
+		data.broadcast.to(message.room).emit("message",{text:message.text,writer:message.userName})
+	})
+
+
+})
+
+server.listen(8080,()=>console.log("listening at port 8080"))
